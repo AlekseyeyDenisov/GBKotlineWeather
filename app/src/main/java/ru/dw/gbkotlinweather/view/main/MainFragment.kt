bd16e87704d11a1f7b3fb7935efd1a1ec240812e
77den <77denisov@gmail.com>
2022-03-27 12:58:47 +0300

    create  Binding MainFragment.kt

--- app/src/main/java/ru/dw/gbkotlinweather/view/main/MainFragment.kt
+++ app/src/main/java/ru/dw/gbkotlinweather/view/main/MainFragment.kt
@@ -1,22 +1,22 @@
 package ru.dw.gbkotlinweather.view.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.dw.gbkotlinweather.R
-
+import androidx.fragment.app.Fragment
+import ru.dw.gbkotlinweather.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
