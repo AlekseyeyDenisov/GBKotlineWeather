e59de705801b65332f10335f3e91cdd0ae10b185
77den <77denisov@gmail.com>
2022-04-22 09:26:37 +0300

    Домашние задания к уроку №8 Работа над ошибками в WeatherLoader добавление блока try

--- app/src/main/java/ru/dw/gbkotlinweather/repository/api_yandex/WeatherLoader.kt
+++ app/src/main/java/ru/dw/gbkotlinweather/repository/api_yandex/WeatherLoader.kt
@@ -4,8 +4,8 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import ru.dw.gbkotlinweather.model.City
-import ru.dw.gbkotlinweather.model.Weather
 import ru.dw.gbkotlinweather.utils.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -13,7 +13,7 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 
-class WeatherLoader: DetailsRepository {
+class WeatherLoader : DetailsRepository {
 
     override fun getWeatherDetails(city: City, callbackWeather: OnServerResponseListener) {
         val latLong = "?lat=${city.lat}&lon=${city.lon}"
@@ -28,44 +28,53 @@ class WeatherLoader: DetailsRepository {
                     readTimeout = READ_TIMEOUT
                     addRequestProperty(YANDEX_API_KEY, API_KEY)
                 }
-            val headers = urlConnection.headerFields
-            Log.d("@@@", "headers: $headers")
-            val responseCode = urlConnection.responseCode
-            val responseMessage = urlConnection.responseMessage
-            Log.d("@@@", "responseMessage: $responseMessage")
+            try {
 
-            val maximumNumberError = 600..2000
-            val serverside = 500..599
-            val clientside = 400..499
-            val responseOk = 200..299
 
-            when (responseCode) {
-                in maximumNumberError -> {
-                    callbackWeather.error("Не известная ошибка $maximumNumberError")
-                    Log.d("@@@", "Не известная ошибка $maximumNumberError")
-                }
-                in serverside -> {
-                    Log.d("@@@", "serverside: $serverside")
-                    callbackWeather.error(responseMessage)
-                }
-                in clientside -> {
-                    Log.d("@@@", "clientside: $clientside")
-                    callbackWeather.error(responseMessage)
+                val headers = urlConnection.headerFields
+                Log.d("@@@", "headers: $headers")
+                val responseCode = urlConnection.responseCode
+                val responseMessage = urlConnection.responseMessage
+                Log.d("@@@", "responseMessage: $responseMessage")
 
-                }
-                in responseOk -> {
-                    val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
-                    val weatherDto: WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
-                    val weather = convertDtoToModel(weatherDto).apply {
-                        this.city = city
+                val maximumNumberError = 600..2000
+                val serverside = 500..599
+                val clientside = 400..499
+                val responseOk = 200..299
+
+                when (responseCode) {
+                    in maximumNumberError -> {
+                        callbackWeather.error("Не известная ошибка $maximumNumberError")
+                        Log.d("@@@", "Не известная ошибка $maximumNumberError")
                     }
-                    Log.d("@@@", "getCityWeather: $weather")
-                    Handler(Looper.getMainLooper()).post {
-                        callbackWeather.onResponseSuccess(weather)
+                    in serverside -> {
+                        Log.d("@@@", "serverside: $serverside")
+                        callbackWeather.error(responseMessage)
+                    }
+                    in clientside -> {
+                        Log.d("@@@", "clientside: $clientside")
+                        callbackWeather.error(responseMessage)
+
+                    }
+                    in responseOk -> {
+                        val buffer = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                        val weatherDto: WeatherDTO = Gson().fromJson(buffer, WeatherDTO::class.java)
+                        val weather = convertDtoToModel(weatherDto).apply {
+                            this.city = city
+                        }
+                        Log.d("@@@", "getCityWeather: $weather")
+                        Handler(Looper.getMainLooper()).post {
+                            callbackWeather.onResponseSuccess(weather)
+                        }
                     }
                 }
+            } catch (e: JsonSyntaxException) {
+                e.message?.let { callbackWeather.error(it) }
+
+            } finally {
+                urlConnection.disconnect()
             }
-            urlConnection.disconnect()
+
 
         }.start()
     }
