40b248ec867e0ba9da898774fe4515c1de56b8f5
77den <77denisov@gmail.com>
2022-04-30 15:14:46 +0300

    Домашние задания к уроку №10 Исправление ошибки в ContactUserViewModel

--- app/src/main/java/ru/dw/gbkotlinweather/view/contacts/ContactUserViewModel.kt
+++ app/src/main/java/ru/dw/gbkotlinweather/view/contacts/ContactUserViewModel.kt
@@ -18,30 +18,31 @@ class ContactUserViewModel(application: Application) : AndroidViewModel(applicat
 
     @SuppressLint("Range")
     fun loadPhoneContact() {
+        if (liveDate.value == null) {
+            val contentResolver: ContentResolver = context.contentResolver
+            val cursor = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
 
-        val contentResolver: ContentResolver = context.contentResolver
-        val cursor = contentResolver.query(
-            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-            null,
-            null,
-            null,
-            null
-        )
+            cursor?.let { cur ->
+                while (cur.moveToNext()) {
+                    val columnIndexName = cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                    val name = cur.getString(columnIndexName)
+                    val phone =
+                        cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                    listUserContact.add(UserContact(name, phone))
 
-        cursor?.let { cur ->
-            while (cur.moveToNext()) {
-                val columnIndexName = cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
-                val name = cur.getString(columnIndexName)
-                val phone =
-                    cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                listUserContact.add(UserContact(name,phone))
-
+                }
+                liveDate.postValue(listUserContact)
             }
-            liveDate.postValue(listUserContact)
+            cursor?.close()
+
+
         }
-        cursor?.close()
-
-
     }
 
 
